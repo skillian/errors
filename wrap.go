72a8953d4d047bda1dcc6829bc8e255554f1a79b
Sean Killian <skillian92@gmail.com>
2018-06-11 20:15:22 -0400

    Create wrap.go
    Untested version of WrapDeferred.

---
+++ wrap.go
@@ -0,0 +1,20 @@
+package errors
+
+// WrapDeferred wraps a deferred function and captures its error into the
+// provided error pointer.
+func WrapDeferred(pErr *error, f func() error) {
+	if pErr == nil {
+		panic("pErr error pointer is nil")
+	}
+	err := f()
+	if err != nil {
+		if *pErr == nil {
+			*pErr = err
+		} else {
+			*pErr = Error{
+				Err: err,
+				Context: *pErr,
+			}
+		}
+	}
+}
